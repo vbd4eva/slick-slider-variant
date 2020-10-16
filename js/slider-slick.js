619abe953bbb494ca377d09d2132a2d4d207c2e5
Denis Onyshchuk <o.82.denis@gmail.com>
2020-10-17 00:33:52 +0300

    slider control buttons design - pre-changes

--- js/slider-slick.js
+++ js/slider-slick.js
@@ -1,3 +1,24 @@
 $(document).ready(function () {
-  $('.client-slider').slick();
+
+    $('.client-slider').slick();
+
+  const clientSlider = document.querySelector('.clients');
+
+  clientSlider.addEventListener("click", animateArrow);
+
+
+
+
 });
+
+
+function animateArrow(e) { 
+  if(!e.target.classList.contains('slick-arrow')) return false;
+ 
+  e.target.classList.add("motion");
+
+  setTimeout(() => { e.target.classList.remove("motion"); }, 1000);
+  
+
+}
+
